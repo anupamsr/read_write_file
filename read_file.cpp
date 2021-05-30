97e733172a0d6bf1343c099ac0d2518e7174a5a5
Anupam Srivastava <anupam@dont.tech>
2021-05-30 16:55:05 +0530

    Converted inotify to class

--- read_file.cpp
+++ read_file.cpp
@@ -17,6 +17,99 @@ void check_error(const int descriptor, const string& msg)
     }
 }
 
+class Inotify {
+public:
+    class iterator {
+        using value_type = inotify_event;
+        using pointer = value_type*;
+
+    public:
+        iterator(const char* _buffer, const size_t& _i, const size_t& _length)
+            : buffer(_buffer)
+            , i(_i)
+            , length(_length)
+            , value_ptr((inotify_event*)&buffer[i])
+        {
+        }
+
+        iterator(const iterator&) = delete;
+
+        iterator& operator=(const iterator&) = delete;
+
+        iterator(iterator&& it)
+        {
+            *this = std::move(it);
+        }
+
+        iterator& operator=(iterator&& it)
+        {
+            if (this != &it) {
+                buffer = std::move(it.buffer);
+                i = std::move(it.i);
+                length = std::move(it.length);
+                value_ptr = std::move(it.value_ptr);
+            }
+            return *this;
+        }
+
+        pointer operator->()
+        {
+            return value_ptr;
+        }
+
+        bool operator!=(const iterator& it)
+        {
+            return buffer != it.buffer || i != it.i;
+        }
+
+        void operator++()
+        {
+            i = min(i + EVENT_SIZE + value_ptr->len, length);
+            value_ptr = (inotify_event*)&buffer[i];
+        }
+
+    private:
+        const char* buffer = nullptr;
+        size_t i;
+        size_t length;
+        pointer value_ptr = nullptr;
+    };
+
+    Inotify(const string& file, const uint32_t& mask)
+        : fd(inotify_init())
+        , wd(inotify_add_watch(fd, file.c_str(), mask))
+    {
+        check_error(fd, "inotify_init");
+        check_error(wd, "inotify_add_watch");
+    }
+
+    iterator wait()
+    {
+        length = read(fd, buffer, EVENT_BUF_LEN);
+        check_error(length, "read");
+        return iterator(buffer, 0, length);
+    }
+
+    iterator begin() const = delete;
+
+    iterator end() const
+    {
+        return iterator(buffer, length, length);
+    }
+
+    ~Inotify()
+    {
+        inotify_rm_watch(fd, wd);
+        close(fd);
+    }
+
+private:
+    int fd;
+    int wd;
+    char buffer[EVENT_BUF_LEN] = { 0 };
+    int length;
+};
+
 int main()
 {
     try {
@@ -26,41 +119,32 @@ int main()
         while (getline(ifs, line)) {
             cout << "Read: " << line << endl;
         }
-        auto fd = inotify_init();
-        check_error(fd, "inotify_init");
-        auto wd = inotify_add_watch(fd, FILE_TO_READ, IN_MODIFY | IN_CLOSE | IN_OPEN);
-        check_error(wd, "inotify_add_watch");
+        Inotify inotify(FILE_TO_READ, IN_MODIFY | IN_CLOSE | IN_OPEN);
         bool closed = false;
         while (!closed) {
-            char buffer[EVENT_BUF_LEN];
-            auto length = read(fd, buffer, EVENT_BUF_LEN);
-            check_error(length, "read");
-            int i = 0;
-            while (i < length) {
-                auto* event = (struct inotify_event*)&buffer[i];
-                if (event->mask & IN_OPEN) {
+            for (auto it = inotify.wait(); it != inotify.end(); ++it) {
+                if (it->mask & IN_OPEN) {
                     cerr << "read_file should be opened during or after file is modified, not before" << endl;
                     closed = true;
-                } else if (event->mask & IN_MODIFY) {
+                    break;
+                } else if (it->mask & IN_MODIFY) {
                     while (getline(ifs, line)) {
                         cout << "Read: " << line << endl;
                     }
                     ifs.clear();
-                } else if (event->mask & IN_CLOSE) {
+                } else if (it->mask & IN_CLOSE) {
                     closed = true;
+                    break;
                 } else {
-                    if (event->mask & IN_ISDIR) {
-                        cout << "Directory " << event->name << " had uncatched event.\n";
+                    if (it->mask & IN_ISDIR) {
+                        cout << "Directory " << it->name << " had uncatched event.\n";
                     } else {
-                        cout << "File " << event->name << " had uncatched event.\n";
+                        cout << "File " << it->name << " had uncatched event.\n";
                     }
                 }
-                i += EVENT_SIZE + event->len;
             }
             check_error(ifs.bad() ? -1 : 0, (string("error while reading file ") + FILE_TO_READ));
         }
-        inotify_rm_watch(fd, wd);
-        close(fd);
         ifs.close();
     } catch (...) {
         return -1;
